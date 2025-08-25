980ca4667a8b629a2a19d7d1de6e0bcf648aa100
Oxsitoo <fsgarciah@academia.usbbog.edu.co>
2025-08-25 21:32:10 +0000

    Retyo java 3 finished

--- java_retos/reto3_buscar_booleans/Reto3.java
+++ java_retos/reto3_buscar_booleans/Reto3.java
@@ -9,7 +9,7 @@ import java.util.regex.*;
 
 public class Reto3 {
     public static void main(String[] args) {
-        String texto = "La respuesta es True, pero a veces es False.";
+        String texto = "La respuesta es True, pero a veces es False, pero siempre hablo con la True";
         // Expresión regular para booleanos (True o False, case-insensitive)
         String patron = "\\b(True|False)\\b";
         Pattern pattern = Pattern.compile(patron, Pattern.CASE_INSENSITIVE);
